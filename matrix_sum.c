b0cbcdc3a171f18d2bb21c8c6cede13e56fc9363
Christian Magnerfelt <christian.magnerfelt@gmail.com>
2013-02-15 11:58:08 +0100

    Updated matrix sum to use a bag of task

--- matrix_sum.c
+++ matrix_sum.c
@@ -32,9 +32,9 @@
 #define MAXSIZE 10000  /* maximum matrix size */
 #define MAXWORKERS 10   /* maximum number of workers */
 
-#define DEBUG /* Enable debugging */
+/* #define DEBUG*/ /* Enable debugging */
 
-int g_sum = 0;			/* matrix total sum */
+long long g_sum = 0;	/* matrix total sum */
 int g_min = INT_MAX;	/* matrix minimum value */ 
 int g_minX = -1;		/* matrix minimum value x position */
 int g_minY = -1;		/* matrix minimum value y position */
@@ -42,7 +42,11 @@ int g_max = INT_MIN;	/* matrix maximum value */
 int g_maxX = -1;		/* matrix maximum value x position */
 int g_maxY = -1;		/* matrix maximum value y position */
 
-pthread_mutex_t lock;	/* lock for shared data */
+pthread_mutex_t dataLock;	/* lock for shared data */
+
+int g_count = 0;
+int g_size;
+pthread_mutex_t countLock;
 
 int numWorkers;		/* number of workers */
 
@@ -67,37 +71,35 @@ int main(int argc, char *argv[])
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 	 
 	/* initialize mutex */
-	pthread_mutex_init(&lock, NULL);
-
-	/* read command line args if any */
-	size = (argc > 1)? atoi(argv[1]) : MAXSIZE;
-	numWorkers = (argc > 2)? atoi(argv[2]) : MAXWORKERS;
-	if (size > MAXSIZE) size = MAXSIZE;
-	if (numWorkers > MAXWORKERS) numWorkers = MAXWORKERS;
+	pthread_mutex_init(&dataLock, NULL);
+	pthread_mutex_init(&countLock, NULL);
 	
-	/* Calculate the strip size */
-	stripSize = size/numWorkers;
+	/* read command line args if any */
+	g_size = (argc > 1)? atoi(argv[1]) : MAXSIZE;
+	numWorkers = (argc > 2)? atoi(argv[2]) : MAXWORKERS;
+	if (g_size > MAXSIZE) g_size = MAXSIZE;
+	if (numWorkers > MAXWORKERS) numWorkers = MAXWORKERS;
 
 	/* initialize the matrix */
-	for (i = 0; i < size; i++)
+	for (i = 0; i < g_size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < g_size; j++)
 		{
-			matrix[i][j] = rand()%99;
+			matrix[i][j] = 1; /* rand()%99;*/
 		}
 	}
 	
 	/* print the matrix */
 	#ifdef DEBUG
-	for (i = 0; i < size; i++)
+	/*for (i = 0; i < g_size; i++)
 	{
 		printf("[ ");
-		for (j = 0; j < size; j++)
+		for (j = 0; j < g_size; j++)
 		{
 	    	printf(" %d", matrix[i][j]);
 	    }
 		printf(" ]\n");
-	}
+	}*/
 	#endif
 
 	/* do the parallel work: create the workers */
@@ -106,6 +108,7 @@ int main(int argc, char *argv[])
 	{
 		pthread_create(&workerid[l], &attr, Worker, (void *) l);
 	}
+	
 	/* join all worker threads*/
 	for (l = 0; l < numWorkers; l++)
 	{
@@ -121,11 +124,16 @@ int main(int argc, char *argv[])
       		l, workerid[l], (long)status);
       	#endif		
 	}
+	
+	/* Destroy mutexes which are not needed anymore */
+	pthread_mutex_destroy(&dataLock);
+	pthread_mutex_destroy(&countLock);	
 
 	/* print results */
-	printf("The total is %d\n", g_sum);
+	printf("The total is %llu\n", g_sum);
 	printf("The min element is %d at (%d , %d)\n", g_min, g_minX, g_minY);
 	printf("The max element is %d at (%d , %d)\n", g_max, g_maxX, g_maxY);
+	printf("%d number of rows processed\n", g_count);	
 		
 	/* get end time */
 	endTime = readTimer();
@@ -138,59 +146,73 @@ int main(int argc, char *argv[])
 void *Worker(void *arg)
 {
 	long myid = (long) arg;
-	int total, min, minX, minY, max, maxX, maxY, i, j, first, last;
+	int total, min, minX, minY, max, maxX, maxY, i, row;
+	bool done = false;
 	
 	#ifdef DEBUG
 		printf("worker %ld (pthread id %lu) has started\n", myid, pthread_self());
 	#endif
 	
-	/* determine first and last rows of my strip */
-	first = myid*stripSize;
-	last = (myid == numWorkers - 1) ? (size - 1) : (first + stripSize - 1);
+	while(!done)
+	{
+		pthread_mutex_lock(&countLock);
+		if(g_count < g_size)
+		{
+			row = g_count; 
+			g_count++;
+			#ifdef DEBUG
+			printf("worker %ld (pthread id %lu) working on row %d\n", myid, pthread_self(), row);
+			#endif
+		}
+		else
+		{
+			done = true;
+		}
+		pthread_mutex_unlock(&countLock);
+		
+		if(done)
+			break;
+		
+		/* sum values in my row */
+		total = 0;
+		min = INT_MAX;
+		max = INT_MIN;
+		for (i = 0; i < g_size; i++)
+		{
+			if(matrix[row][i] < min)
+			{
+				min = matrix[row][i];
+				minX = i;
+				minY = row;
+			}
+			else if(matrix[row][i] > max)
+			{
+				max = matrix[row][i];
+				maxX = i;
+				maxY = row;
+			}
+	  		total += matrix[row][i];
+	  	}
+		pthread_mutex_lock(&dataLock);
 
-	/* sum values in my strip */
-	total = 0;
-	min = INT_MAX;
-	max = INT_MIN;
-	for (i = first; i <= last; i++)
-	{
-    	for (j = 0; j < size; j++)
-    	{
-    		if(matrix[i][j] < min)
-    		{
-    			min = matrix[i][j];
-    			minX = j;
-    			minY = i;
-    		}
-    		else if(matrix[i][j] > max)
-    		{
-    			max = matrix[i][j];
-    			maxX = j;
-    			maxY = i;
-    		}
-      		total += matrix[i][j];
-      	}
-	}
-	pthread_mutex_lock(&lock);
-	
-	/* save max and min value for this worker and their respective position */
-	if(min < g_min)
-	{
-		g_min = min;
-		g_minX = minX;
-		g_minY = minY;
-	}
-	if(max > g_max)
-	{
-		g_max = max;
-		g_maxX = maxX;
-		g_maxY = maxY;	
-	}
-	/* add partial sum to global sum */
-	g_sum += total;
+		/* save max and min value for this worker and their respective position */
+		if(min < g_min)
+		{
+			g_min = min;
+			g_minX = minX;
+			g_minY = minY;
+		}
+		if(max > g_max)
+		{
+			g_max = max;
+			g_maxX = maxX;
+			g_maxY = maxY;	
+		}
+		/* add partial sum to global sum */
+		g_sum += total;
 
-	pthread_mutex_unlock(&lock);
-	
+		pthread_mutex_unlock(&dataLock);
+	}
 	pthread_exit(0);
 }
 /* timer */
